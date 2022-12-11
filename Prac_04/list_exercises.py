2a12fe95d5ae7529ec3478a95c4a4391d60156a5
NammanC137 <102642165+JC827574@users.noreply.github.com>
2022-12-11 17:44:44 +0800

    Added the username validator

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -13,4 +13,13 @@ print("The lowest number you entered was {}".format(min(numbers)))
 print("the highest number you entered was {}".format(max(numbers)))
 print("the average of the numbers you entered was {}".format(sum(numbers)/len(numbers)))
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+input_username = input("Enter your username: ")
+
+if input_username in usernames:
+    print("Access Granted!")
+else:
+    print("Access Denied")
 
