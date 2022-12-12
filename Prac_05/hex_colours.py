57d34d8e7a765d8906e183c135d153ecb47d622f
NammanC137 <102642165+JC827574@users.noreply.github.com>
2022-12-12 19:53:13 +0800

    Add more colors to the dictionary !

--- Prac_05/hex_colours.py
+++ Prac_05/hex_colours.py
@@ -5,12 +5,14 @@ Finding the code for the color
 """
 
 CODE_OF_COLOR = {"asparagus": "#0048ba", "acid green": "#b0bf1a", "matcha green": "#69bf64", "bright green": "#66ff00",
-                 "brunswick green": "#1b4d3e", "cadmium green": "#006b3c"}
-
-COLOR_OPTIONS = " Asparagus\n Acid Green\n Matcha Green\n Bright Green\n Brunswick Green\n Cadmium Green"
+                 "brunswick green": "#1b4d3e", "cadmium green": "#006b3c", "caribbean green": "	#00cc99",
+                 "camouflage green": "#78866b", "castleton green": "#00563f", "celadon": "#ace1af"}
+print("Hex Color Green: Choose a color to find it's hex code !")
+COLOR_OPTIONS = " Asparagus\n Acid Green\n Matcha Green\n Bright Green\n Brunswick Green\n Cadmium Green\n Caribbean " \
+                "Green\n Camouflage Green\n Castleton Green\n Celadon "
 print(COLOR_OPTIONS)
 
-userInput = input("Choose a color from the color bar above(^) : ").lower()
+userInput = input("Your Choice: ").lower()
 while userInput != "":
     if userInput in CODE_OF_COLOR:
         print("{} has the code: {}".format(userInput, CODE_OF_COLOR[userInput]))
